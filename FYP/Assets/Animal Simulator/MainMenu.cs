ffdc0eecb6a802be8aa64e873655bed32f504a64
Zhimianwu1998 <zhimianwuchina@gmail.com>
2020-12-16 21:35:25 +0000

    prototype

---
+++ FYP/Assets/Animal Simulator/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("PLAY Clicked");
+        Application.Quit();
+    }
+    
+    public void GoToSettingMenu()
+    {
+        Debug.Log("SETTINGS Clicked");
+        SceneManager.LoadScene("SettingMenu");
+    }
+
+    public void GoToMainMenu()
+    {
+        Debug.Log("Back Clicked");
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT Clicked");
+        Application.Quit();
+    }
+}
